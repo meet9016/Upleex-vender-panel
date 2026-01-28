a2142b408fa9610cd1c6a745fe9ea600b474ee9d
meet <meet.shopno@gmail.com>
2026-01-28 15:15:40 +0530

    axios instatance change

--- src/utils/axiosInstance.tsx
+++ src/utils/axiosInstance.tsx
@@ -31,14 +31,14 @@ apiAdminInstance.interceptors.response.use(
 
     // if (response.status === 401) {
     //   localStorage.removeItem('auth_token');
-    //   window.location.href = '/login';
+    //   window.location.href = '/signin';
     // }
 
      if (response?.status === 401) {
       // optional: avoid infinite redirect
-      if (window.location.pathname !== '/login') {
+      if (window.location.pathname !== '/signin') {
         localStorage.removeItem('auth_token');
-        window.location.replace('/login');
+        window.location.replace('/signin');
       }
     }
     return Promise.reject(error);
