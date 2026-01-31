3434347d6b1d922ebbe9d36d78e708373a5714d1
yogeshShopno <yogesh.shopno@gmail.com>
2026-01-31 17:44:33 +0530

    prevent accessing private pages to login

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -4,7 +4,8 @@ import { useSidebar } from "@/context/SidebarContext";
 import AppHeader from "@/layout/AppHeader";
 import AppSidebar from "@/layout/AppSidebar";
 import Backdrop from "@/layout/Backdrop";
-import React from "react";
+import {  useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 export default function AdminLayout({
   children,
@@ -13,12 +14,19 @@ export default function AdminLayout({
 }) {
   const { isExpanded, isHovered, isMobileOpen } = useSidebar();
 
+    const router = useRouter();
+
   // Dynamic class for main content margin based on sidebar state
   const mainContentMargin = isMobileOpen
     ? "ml-0"
     : isExpanded || isHovered
     ? "lg:ml-[290px]"
     : "lg:ml-[90px]";
+
+    useEffect(() => {
+  const token = localStorage.getItem("auth_token");
+  if (!token) router.push("/signin");
+}, []);
 
   return (
     <div className="min-h-screen xl:flex">
