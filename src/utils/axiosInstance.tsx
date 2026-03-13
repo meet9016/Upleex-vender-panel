adcd9b77b93e417d03e1bc39614b2b6d429ece13
meet <meet.shopno@gmail.com>
2026-03-13 17:27:26 +0530

    axios file change

--- src/utils/axiosInstance.tsx
+++ src/utils/axiosInstance.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const apiAdminInstance = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  baseURL: process.env.NEXT_PUBLIC_APP_URL
+const apiAdminInstance = axios.create({  
+  // baseURL: process.env.NEXT_PUBLIC_APP_URL
+  baseURL: 'https://upleex.com/api/api/v1/',
   // headers: {
   //   'Content-Type': 'multipart/form-data'
   // }
